26ca5f57484b6a8c841d813e52d49e5662b46a7c
xfmeng17 <xfmeng17@foxmail.com>
2019-04-11 20:06:51 +0800

    feat(3-11): finish lesson 3-11

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,48 @@
-Page({})
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
+Page({
+  data: {
+    nowTemp: 14,
+    nowWeather: '多云',
+    nowWeatherBackground: ''
+  },
+  onLoad() {
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now',
+      data: {
+        city: '北京市'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res => {
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        console.log(temp, weather)
+        this.setData({
+          nowTemp: temp + '°',
+          nowWeather: weatherMap[weather]
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
+      }
+    })
+  }
+})
