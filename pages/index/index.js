0e088ca02974bbf7e2d1f3d96473b1c1aca66c6d
xfmeng17 <xfmeng17@foxmail.com>
2019-04-14 13:57:47 +0800

    feat(lesson6): implement onShow() handle setting change
    Use this commit to practice code rollback

--- pages/index/index.js
+++ pages/index/index.js
@@ -16,6 +16,15 @@ const weatherColorMap = {
   'snow': '#aae1fc'
 }
 
+const QQMapWX = require('../../libs/qqmap-wx-jssdk.js')
+
+const UNPROMPTED = 0
+const UNAUTHORIZED = 1
+const AUTHORIZED = 2
+const UNPROMPTED_TIPS = '点击获取当前位置'
+const UNAUTHORIZED_TIPS = '点击开启位置权限'
+const AUTHORIZED_TIPS = ''
+
 Page({
   data: {
     nowTemp: '',
@@ -23,24 +32,82 @@ Page({
     nowWeatherBackground: "",
     hourlyWeather: [],
     todayDate: '',
-    todayTemp: ''
+    todayTemp: '',
+    city: '上海市',
+    locationAuthType: UNPROMPTED,
+    locationTipsText: UNPROMPTED_TIPS
   },
 
-  onLoad() {
+  onLoad () {
+    console.log('index.js onLoad()!')
+    this.qqmapsdk = new QQMapWX({
+      key: 'GLUBZ-BWXKX-W6F4R-ZO2WF-XD3FS-AUFHK'
+    })
     this.getNow()
   },
 
-  onPullDownRefresh(){
+  onShow () {
+    console.log('index.js onShow()!')
+    wx.getSetting({
+      success: res => {
+        let auth = res.authSetting['scope.userLocation']
+        // console.log(res)
+        // console.log(auth)
+        if (auth && this.data.locationAuthType != AUTHORIZED) {
+          this.setData({
+            locationAuthType: AUTHORIZED,
+            locationTipsText: AUTHORIZED_TIPS
+          })
+          this.getLocation()
+        }
+      }
+    })
+  },
+
+  onReady () {
+    // Do something when page ready.
+    console.log('index.js onReady()!')
+  },
+  onHide() {
+    // Do something when page hide.
+    console.log('index.js onHide()!')
+  },
+  onUnload() {
+    // Do something when page close.
+    console.log('index.js onUnload()!')
+  },
+  onPullDownRefresh() {
+    // Do something when pull down.
+    console.log('index.js onPullDownRefresh()!')
+  },
+  onReachBottom() {
+    // Do something when page reach bottom.
+    console.log('index.js onReachBottom()!')
+  },
+  onShareAppMessage() {
+    // return custom share data when user share.
+    console.log('index.js onShareAppMessage()!')
+  },
+  onPageScroll() {
+    // Do something when page scroll.
+    console.log('index.js onPageScroll()!')
+  },
+  onResize() {
+    // Do something when page resize.
+    console.log('index.js onResize()!')
+  },
+
+  onPullDownRefresh () {
     this.getNow(() => {
       wx.stopPullDownRefresh()
     })
   },
 
-  getNow(callback){
+  getNow (callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/now',
       data: {
-        city: '北京市'
+        city: this.data.city
       },
       success: res => {
         let result = res.data.result
@@ -96,7 +163,48 @@ Page({
   onTapDayWeather () {
     // wx.showToast()
     wx.navigateTo({
-      url: '/pages/list/list'
+      url: '/pages/list/list?city=' + this.data.city
+    })
+  },
+
+  onTapLocation () {
+    if (this.data.locationAuthType === UNAUTHORIZED) {
+      wx.openSetting()
+    } else {
+      this.getLocation()
+    }
+  },
+
+  getLocation () {
+    wx.getLocation({
+      success: res => {
+        // console.log(res)
+        this.setData({
+          locationAuthType: AUTHORIZED,
+          locationTipsText: AUTHORIZED_TIPS
+        })
+        this.qqmapsdk.reverseGeocoder({
+          location: {
+            latitude: res.latitude,
+            longitude: res.longitude
+          },
+          success: res => {
+            let city = res.result.address_component.city
+            // console.log(city)
+            this.setData({
+              city: city,
+              // locationTipsText: ''
+            })
+            this.getNow()
+          }
+        })
+      },
+      fail: () => {
+        this.setData({
+          locationAuthType: UNAUTHORIZED,
+          locationTipsText: UNAUTHORIZED_TIPS
+        })
+      }
     })
   }
 })
--- pages/list/list.js
+++ pages/list/list.js
@@ -2,22 +2,68 @@ const dayMap = ['星期日', '星期一', '星期二', '星期三', '星期四',
 
 Page({
   date: {
-    weekWeather: []
+    weekWeather: [],
+    city: '北京市'
   },
-  onLoad() {
+
+  onLoad (options) {
+    console.log('list.js onLoad()!')
+    this.setData({
+      city: options.city
+    })
     this.getWeekWeather()
   },
+
+  onReady() {
+    // Do something when page ready.
+    console.log('list.js onReady()!')
+  },
+  onShow () {
+    // Do something when page show.
+    console.log('list.js onShow()!')
+  },
+  onHide() {
+    // Do something when page hide.
+    console.log('list.js onHide()!')
+  },
+  onUnload() {
+    // Do something when page close.
+    console.log('list.js onUnload()!')
+  },
+  onPullDownRefresh() {
+    // Do something when pull down.
+    console.log('list.js onPullDownRefresh()!')
+  },
+  onReachBottom() {
+    // Do something when page reach bottom.
+    console.log('list.js onReachBottom()!')
+  },
+  onShareAppMessage() {
+    // return custom share data when user share.
+    console.log('list.js onShareAppMessage()!')
+  },
+  onPageScroll() {
+    // Do something when page scroll
+    console.log('list.js onPageScroll()!')
+  },
+  onResize() {
+    // Do something when page resize
+    console.log('list.js onResize()!')
+  },
+  
+
   onPullDownRefresh() {
     this.getWeekWeather(() => {
       wx.stopPullDownRefresh()
     })
   },
+
   getWeekWeather(callback) {
     wx.request({
       url: 'https://test-miniprogram.com/api/weather/future',
       data: {
         time: new Date().getTime(),
-        city: "北京市"
+        city: this.data.city
       },
       success: res => {
         let result = res.data.result
@@ -28,6 +74,7 @@ Page({
       }
     })
   },
+
   setWeekWeather(result) {
     let weekWeather = []
     for (let i = 0; i < 7; i++) {
